a27fc60fd51c0a3af62fccb807df1bb19e706ae3
Your Name <you@example.com>
2024-07-15 18:16:20 +0300

    add 3 first labs

--- labs/03-01-variables/start.sh
+++ labs/03-01-variables/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo clean-working-dir
+sudo mv /labs/03-01-variables/assets/prepare-and-deploy /home/moon; sudo chown moon:moon /home/moon/prepare-and-deploy; sudo chmod +x /home/moon/prepare-and-deploy
