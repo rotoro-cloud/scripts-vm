bcf455b19778a2ee967bb92ddba60fd7f82e44a5
Your Name <you@example.com>
2024-07-15 17:36:27 +0300

    add 3 first labs

--- labs/02-01-first-script/start.sh
+++ labs/02-01-first-script/start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 sudo clean-working-dir
-sudo mv /labs/02-01-first-script/assets/ /root/
--- labs/03-01-variables/start.sh
+++ labs/03-01-variables/start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 sudo clean-working-dir
-sudo mv /labs/03-01-variables/assets/ /root/
