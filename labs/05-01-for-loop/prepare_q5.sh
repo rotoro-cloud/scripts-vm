2f8c21f10e2f781413bae3cf87544c27fcd84790
Your Name <you@example.com>
2024-07-16 13:06:23 +0300

    add 7 lab

--- labs/05-01-for-loop/prepare_q5.sh
+++ labs/05-01-for-loop/prepare_q5.sh
@@ -2,6 +2,6 @@
 
 
 cp /labs/05-01-for-loop/assets/apps.txt /home/moon/; cp /labs/05-01-for-loop/assets/count-requests.sh /home/moon/count-requests.sh;
-chmod +x /home/moon/count-requests.sh; chown moon:moon /home/moon/count-requests.sh; mkdir -p /var/log/apps/; 
+chmod +x /home/moon/count-requests.sh; chown moon:moon /home/moon/count-requests.sh; sudo mkdir -p /var/log/apps/; sudo chown moon:moon /var/log/apps/; 
 
 cp /labs/05-01-for-loop/assets/ci-runners_app.log /labs/05-01-for-loop/assets/federation_app.log /labs/05-01-for-loop/assets/manager_app.log /labs/05-01-for-loop/assets/object-store_app.log /var/log/apps/;
--- labs/05-01-for-loop/test_q5.sh
+++ labs/05-01-for-loop/test_q5.sh
@@ -6,25 +6,25 @@ if [ $? -ne 0 ]; then
     clear &&  echo "Скрипт не использует цикл for" && exit 1
 fi
 
-cat /home/moon/count-requests.sh | grep manager | grep 17 | grep 16 | grep 20 | grep 15 | grep 13
+bash /home/moon/count-requests.sh | grep manager | grep 17 | grep 16 | grep 20 | grep 15 | grep 13
 
 if [ $? -ne 0 ]; then
     clear &&  echo "Скрипт работает неверно с 'manager'" && exit 1
 fi
 
-cat /home/moon/count-requests.sh | grep ci-runners | grep 13 | grep 6 | grep 14 | grep 11 | grep 5 | grep 10
+bash /home/moon/count-requests.sh | grep ci-runners | grep 13 | grep 6 | grep 14 | grep 11 | grep 5 | grep 10
 
 if [ $? -ne 0 ]; then
     clear &&  echo "Скрипт работает неверно с 'ci-runners'" && exit 1
 fi
 
-cat /home/moon/count-requests.sh | grep object-store | grep 78 | grep 76 | grep 90 | grep 100 | grep 89 | grep 66
+bash /home/moon/count-requests.sh | grep object-store | grep 78 | grep 76 | grep 90 | grep 100 | grep 89 | grep 66
 
 if [ $? -ne 0 ]; then
     clear &&  echo "Скрипт работает неверно с 'object-store'" && exit 1
 fi
 
-cat /home/moon/count-requests.sh | grep ci-runners | grep federation | grep 6 | grep 0 | grep 9 | grep 5 | grep 8
+bash /home/moon/count-requests.sh | grep federation | grep 6 | grep 0 | grep 9 | grep 5 | grep 8
 
 if [ $? -ne 0 ]; then
     clear &&  echo "Скрипт работает неверно с 'federation'" && exit 1
