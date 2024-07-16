698bf930ef8fa2f838dced5565c387cc0f52edca
Your Name <you@example.com>
2024-07-16 13:10:10 +0300

    add 7 lab

--- labs/05-01-for-loop/test_q1.sh
+++ labs/05-01-for-loop/test_q1.sh
@@ -2,13 +2,13 @@
 
 echo "Внимание, тестирование с циклами может занимать до минуты"
 
-sh -c "export PATH=$PATH:/home/moon; labtest multi-launch.sh" | awk -v RS='^$' 'END{exit !(index($0,"success") && index($0,"db-subsystem") && index($0,"backend") && index($0,"frontend") && index($0,"load-balancer") && index($0,"monitoring"))} '
+sh -c "export PATH=$PATH:/home/moon; labtest multi-deploy.sh" | awk -v RS='^$' 'END{exit !(index($0,"success") && index($0,"db-subsystem") && index($0,"backend") && index($0,"frontend") && index($0,"load-balancer") && index($0,"monitoring"))} '
 
 if [ $? -ne 0 ]; then
     clear &&  echo "Скрипт не работает как ожидалось" && exit 1
 fi
 
-cat /home/moon/multi-launch.sh | grep for
+cat /home/moon/multi-deploy.sh | grep for
 
 if [ $? -ne 0 ]; then
     clear &&  echo "Скрипт не использует for" && exit 1
