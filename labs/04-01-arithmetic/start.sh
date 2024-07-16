6c866f625ecb9b59a51eb24c78807891d26eb2f6
Your Name <you@example.com>
2024-07-16 11:02:42 +0300

    add 5 lab

--- labs/04-01-arithmetic/start.sh
+++ labs/04-01-arithmetic/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo clean-working-dir
-sudo mv /labs/03-02-input/assets/calculation.sh /home/moon; sudo chown moon:moon /home/moon/calculation.sh; sudo chmod +x /home/moon/calculation.sh
+sudo mv /labs/04-01-arithmetic/assets/calculation.sh /home/moon; sudo chown moon:moon /home/moon/calculation.sh; sudo chmod +x /home/moon/calculation.sh
