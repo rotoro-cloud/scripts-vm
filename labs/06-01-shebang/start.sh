553e04bbe62f731cc658a23166ad65ebed08261d
Your Name <you@example.com>
2024-07-17 11:36:16 +0300

    add .\labs\06-01-shebang"

--- labs/06-01-shebang/start.sh
+++ labs/06-01-shebang/start.sh
@@ -2,6 +2,6 @@
 sudo clean-working-dir
 sudo rm -rf /bin/sh; sudo ln -s /bin/dash /bin/sh; 
 
-sudo mv /labs/06-01-shebang/assets/assets/loop.sh /home/moon; sudo chown -R moon:moon /home/moon/loop.sh; sudo chmod +x /home/moon/loop.sh;
+sudo cp -R /labs/06-01-shebang/assets/loop.sh /home/moon; sudo chown -R moon:moon /home/moon/loop.sh; sudo chmod +x /home/moon/loop.sh;
 
-sudo mv /labs/06-01-shebang/assets/mcq1.txt /home/moon/; sudo chown moon:moon /home/moon/mcq1.txt
+sudo cp -R /labs/06-01-shebang/assets/mcq1.txt /home/moon/; sudo chown moon:moon /home/moon/mcq1.txt
