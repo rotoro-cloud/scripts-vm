287885fc4d3fc25becf08e47b95e4d8c0e505625
Your Name <you@example.com>
2024-07-23 14:17:24 +0300

    add .\labs\09-01-awk"

--- labs/09-01-awk/start.sh
+++ labs/09-01-awk/start.sh
@@ -2,4 +2,4 @@
 sudo clean-working-dir
 sudo cp -R /labs/09-01-awk/assets/get_names.sh /home/moon/get_names.sh; sudo chmod +x /home/moon/get_names.sh; sudo chown moon:moon /home/moon/get_names.sh
 
-sudo cp -R /labs/09-01-awk/assets/home/moon/dataset.csv /home/moon/dataset.csv; sudo chown moon:moon /home/moon/dataset.csv 
+sudo cp -R /labs/09-01-awk/assets/dataset.csv /home/moon/dataset.csv; sudo chown moon:moon /home/moon/dataset.csv 
